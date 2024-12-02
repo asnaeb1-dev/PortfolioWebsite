66ffd440952005a1747693d4c2298e3b954c6bca
Abhigyan Raha <abhigyanraha76@gmail.com>
2024-12-02 09:33:55 +0530

    minor fix

--- src/components/ui/pages/WorkExperiencePage/WorkExperiencePage.jsx
+++ src/components/ui/pages/WorkExperiencePage/WorkExperiencePage.jsx
@@ -9,9 +9,9 @@ const WorkExperiencePage = () => {
   }, [clickedWorkExperience]);
 
   return (
-    <div className='h-[calc(100vh_-_60px)] overflow-hidden p-4 flex flex-col justify-center'>
-      <h1 className='font-extrabold text-3xl pb-2'>Work Experience</h1>
-      <div className='w-full h-full pl-3 overflow-y-auto overflow-x-hidden'>
+    <div className='h-[calc(100vh_-_60px)] overflow-hidden p-4 pl-0 flex flex-col justify-center'>
+      <h1 className='font-extrabold text-3xl pb-2 pl-4'>Work Experience</h1>
+      <div className='w-full h-full pl-4 overflow-y-auto overflow-x-hidden'>
         <WorkExPath workExperience={WorkExperience.workList} onclick={index => setClickedWorkExperience(index)} />
       </div>
     </div>
